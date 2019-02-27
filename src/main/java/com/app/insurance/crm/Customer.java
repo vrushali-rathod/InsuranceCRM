7c38d22bd0edab17120592f011274499a0f28a5d
vrushali-rathod <vrushaligore@gmail.com>
2019-02-27 16:31:28 +0530

    Added Customer

---
+++ src/main/java/com/app/insurance/crm/Customer.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package com.app.insurance.crm;
+
+/**
+ * @author Pinak-PC
+ *
+ */
+public class Customer {
+	public String firstName;
+	public String middleName;
+	public String lastName;
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getMiddleName() {
+		return middleName;
+	}
+	public void setMiddleName(String middleName) {
+		this.middleName = middleName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	
+}
