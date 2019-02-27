2e0a411bb7ed13882f12bc4be4af6fc6fe307b2a
vrushali-rathod <vrushaligore@gmail.com>
2019-02-27 15:54:58 +0530

    Initial commit

---
+++ src/main/java/com/app/insurance/crm/LanuchInsurance.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.app.insurance.crm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Pinak-PC
+ *
+ */
+@SpringBootApplication
+public class LanuchInsurance {
+	public static void main(String[] args) {
+		System.out.println("Hello World.....");
+		SpringApplication.run(LanuchInsurance.class, args);
+		System.out.println("Hello World.....");
+	}
+}
